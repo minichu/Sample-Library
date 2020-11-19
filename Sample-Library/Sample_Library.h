fb4ba6e9f7de582d916d87b363369cebacdc9af8
hyeondo <jsmjhd@naver.com>
2020-11-19 23:46:45 +0900

    init framework project

---
+++ Sample-Library/Sample_Library.h
@@ -0,0 +1,18 @@
+//
+//  Sample_Library.h
+//  Sample-Library
+//
+//  Created by hyeondo on 2020/11/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sample_Library.
+FOUNDATION_EXPORT double Sample_LibraryVersionNumber;
+
+//! Project version string for Sample_Library.
+FOUNDATION_EXPORT const unsigned char Sample_LibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sample_Library/PublicHeader.h>
+
+
